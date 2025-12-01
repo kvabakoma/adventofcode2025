b00205c2a19723f2094bb815f4544a76969fc0d8
Nikola Georgiev <kvabakoma@gmail.com>
2025-12-01 22:51:04 +0200

    01 solved

--- 01/script.py
+++ 01/script.py
@@ -1,11 +1,33 @@
 dial = list(range(100))
 current_key = 50
+result = 0
 
+def calculate_next_key(curr_key, action):
+    action = line.replace("\n", "")
+    # print(curr_key, action)
+        
+    # add / remove key value
+    if action[0] == "L":
+        curr_key -= int(action[1:])
+    elif action[0] == "R":
+        curr_key += int(action[1:])
+        
+    # normalize the key value
+    if curr_key > len(dial) -1:
+        curr_key = curr_key % len(dial)
+    elif curr_key < 0:
+        curr_key = dial[curr_key % len(dial)]
+    
+    return curr_key
+
+# with open("test.txt") as new_file:
 with open("input.txt") as new_file:
     count = 0
     total_length = 0
 
     for line in new_file:
-        count += 1
+        current_key = calculate_next_key(current_key, line)
+        if current_key == 0:
+            result += 1
 
-print("Total n of lines:", count)
+print(result)
