a6859775212b243265e56541f1bbd33a0fed20d3
Nikola Georgiev <kvabakoma@users.noreply.github.com>
2025-12-04 21:04:33 +0200

    03 - WIP

---
+++ 03/script.py
@@ -0,0 +1,45 @@
+total_joltage = 0
+
+# with open("test.txt") as new_file:
+with open("input.txt") as new_file:
+    
+    for line in new_file:
+        last_action_was_on_1 = False
+        result_list = [0, 0, 0]
+        line = line.replace("\n", "")
+        
+        for i, c in enumerate(line):
+            
+            # handle the last action            
+            if c == len(line) -1:
+                if last_action_was_on_1 == True:
+                    result_list[1] = result_list[0]
+                    result_list[0] = result_list[2]
+                continue
+            
+            # if the previous action moved index 1 to 0, force the current number to index 1
+            if last_action_was_on_1 == True:
+                result_list[1] = int(c)
+                last_action_was_on_1 = False
+                continue
+             
+            # reset last action flag
+            last_action_was_on_1 = False
+            
+            # set index 1 if num is higher
+            if int(c) > result_list[1]:
+                result_list[1] = int(c)
+            
+            # move index 1 to index 0 if it is higher
+            if result_list[1] > result_list[0]:
+                result_list[2] = result_list[0]
+                result_list[0] = result_list[1]
+                last_action_was_on_1 = True
+                
+            # print(result_list, end=", ")
+        
+        local_result = int(str(result_list[0]) + str(result_list[1]))
+        print(local_result, ": ", line)
+        total_joltage += local_result
+                
+print("total_joltage", total_joltage)
