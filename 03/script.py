0e7707c83cf0ede78056491a418921b0e46ac930
Nikola Georgiev <kvabakoma@gmail.com>
2025-12-04 22:02:26 +0200

    03 : 01 Solved

--- 03/script.py
+++ 03/script.py
@@ -1,40 +1,27 @@
-total_joltage = 0
-
+total_joltage = 0    
+    
 # with open("test.txt") as new_file:
 with open("input.txt") as new_file:
     
     for line in new_file:
-        last_num_was_highest = False
-        result_list = [0, 0, 0]
+        result_list = [0, 0]
         line = line.replace("\n", "")
         
         for i, c in enumerate(line):
             
             # handle the last action            
-            if c == len(line) -1:
-                if last_num_was_highest == True:
-                    result_list[1] = result_list[0]
-                    result_list[0] = result_list[2]
-                continue
+            if i == len(line) -1:
+                if int(c) > result_list[1]:
+                    result_list[1] = int(c)
             
-            # if the previous action moved index 1 to 0, force the current number to index 1
-            if last_num_was_highest == True:
-                result_list[1] = int(c)
-                last_num_was_highest = False
-                continue
-             
-            # reset last action flag
-            last_num_was_highest = False
-            
-            # set index 1 if num is higher
-            if int(c) > result_list[1]:
-                result_list[1] = int(c)
-            
+            # set index 0 if num is higher 
+            elif int(c) > result_list[0]:
+                result_list[0] = int(c) 
+                result_list[1] = 0
+                
             # move index 1 to index 0 if it is higher
-            if result_list[1] > result_list[0]:
-                result_list[2] = result_list[0]
-                result_list[0] = result_list[1]
-                last_num_was_highest = True
+            elif int(c) > result_list[1]:
+                result_list[1] = int(c)
                 
             # print(result_list, end=", ")
         
@@ -42,4 +29,4 @@ with open("input.txt") as new_file:
         print(local_result, ": ", line)
         total_joltage += local_result
                 
-print("total_joltage", total_joltage)
+print("total_joltage:", total_joltage)
